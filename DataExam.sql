881c675e309532aa7fbf6be488570a3e67b9f5d4
VuongLe <lqv1993@gmail.com>
2024-08-07 08:55:41 +0700

    SQLExam

--- DataExam.sql
+++ DataExam.sql
@@ -34,8 +34,7 @@ CREATE TABLE Employee(
     Salary Money,
     CONSTRAINT fk FOREIGN KEY (DepartId) REFERENCES Department(DepartId)
 )
-
--- Insert 3 records into the Department table
+--3.Insert into above tables at least 3 records per table [3 marks]:
 INSERT INTO Department (DepartId, DepartName, Descriptions)
 VALUES
     (1, 'HR', 'Human Resources Department'),
@@ -43,7 +42,6 @@ VALUES
     (3, 'Finance', 'Finance Department');
 GO
 
--- Insert 3 records into the Employee table with addresses from Vietnam
 INSERT INTO Employee (EmpCode, FirstName, LastName, Birthday, Gender, Address, DepartId, Salary)
 VALUES
     ('E00001', 'Nguyen', 'Van Thuyet', '1980-01-01', 1, '123 Nguyen Trai, Hanoi', 1, 50000.00),
@@ -51,13 +49,13 @@ VALUES
     ('E00003', 'Le', 'Van Cuuong', '1990-07-30', 1, '789 Tran Hung Dao, Hanoi', 3, 55000.00);
 GO
 
--- Increase the salary for all employees by 10% 
+-- 4. Increase the salary for all employees by 10% 
 UPDATE Employee
 SET Salary = Salary * 1.10;
 GO
 
 
--- Add a constraint to ensure that Salary is always greater than 0
+-- 5. Using ALTER TABLE statement to add constraint on Employee table to ensure that salary always greater than 0
 ALTER TABLE Employee
 ADD CONSTRAINT checkksalary
 CHECK (Salary > 0);
